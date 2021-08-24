cb1c042af6b8e0c51ef58beeabfdf079bd6eacfe
willam-gm <will-gm@hotmail.com>
2021-08-24 14:16:43 -0400

    FIx la lsita de mensualidades que debe se desborda haciendo que el diseno de la pantalla correcto se desconfigure

--- app/Views/access/_correcto.php
+++ app/Views/access/_correcto.php
@@ -52,29 +52,35 @@ use App\Models\Mensualidad;
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 text-center">
                     <h5>Mensualidades</h5>
-                    <?php foreach ($mensualidades as $key => $mensualidad) { ?>
-                        <p style="border:1px solid white;padding:10px">
-                            <?=Mensualidad::getMes($mensualidad['month']).' '.$mensualidad['year']?> 
-                        </p>
-                    <?php } ?>
+                    <div style="width:100%;max-height:60vh; overflow-y:hidden;">
+                        <?php foreach ($mensualidades as $key => $mensualidad) { ?>
+                            <p style="border:1px solid white;padding:10px">
+                                <?=Mensualidad::getMes($mensualidad['month']).' '.$mensualidad['year']?> 
+                            </p>
+                        <?php } ?>
+                    </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 text-center">
                     <h5>Casilleros</h5>
-                    <?php foreach ($casilleros as $key => $casillero) { ?>
-                        <p style="border:1px solid white;padding:10px">
-                            N° <?=$casillero['numero']?> <br>
-                            <?=$casillero['saldo'].' Bs.'?> 
-                        </p>
-                    <?php } ?>
+                    <div style="width:100%;max-height:60vh; overflow-y:hidden;">
+                        <?php foreach ($casilleros as $key => $casillero) { ?>
+                            <p style="border:1px solid white;padding:10px">
+                                N° <?=$casillero['numero']?> <br>
+                                <?=$casillero['saldo'].' Bs.'?> 
+                            </p>
+                        <?php } ?>
+                    </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 text-center">
                     <h5>Seminarios</h5>
-                    <?php foreach ($seminarios as $key => $seminario) { ?>
-                        <p style="border:1px solid white;padding:10px">
-                            <?=$seminario['nombre']?> <br>
-                            <?=$seminario['saldo'].' Bs.'?> 
-                        </p>
-                    <?php } ?>
+                    <div style="width:100%;max-height:60vh; overflow-y:hidden;">
+                        <?php foreach ($seminarios as $key => $seminario) { ?>
+                            <p style="border:1px solid white;padding:10px">
+                                <?=$seminario['nombre']?> <br>
+                                <?=$seminario['saldo'].' Bs.'?> 
+                            </p>
+                        <?php } ?>
+                    </div>
                 </div>
             </div>
         <?php } ?>
